4a064d7ff5241580d8081450d780d540e4b2e6d6
yaochung41 <yaochung41@gmail.com>
2022-02-02 18:28:24 +0100

    move app function to inst

--- R/appTEKRABber.R
+++ R/appTEKRABber.R
@@ -25,30 +25,8 @@
 #' )}
 #' 
 appTEKRABber <- function(DEresult, corrRef, corrCompare, metadata) {
-    geneInput <- DEresult$normalized_gene_counts
-    geneRes <- DEresult$gene_res
-    teInput <- DEresult$normalized_te_counts
-    teRes <- DEresult$te_res
-    
-    rownames(geneInput) <- gsub("[()]", "", rownames(geneInput))
-    rownames(teInput) <- gsub("[()]", "", rownames(teInput))
-    rownames(geneRes) <- gsub("[()]", "", rownames(geneRes))
-    rownames(teRes) <- gsub("[()]", "", rownames(teRes))
-    corrRef$geneName <- gsub("[()]", "", corrRef$geneName)
-    corrRef$teName <- gsub("[()]", "", corrRef$teName)
-    corrCompare$geneName <- gsub("[()]", "", corrCompare$geneName)
-    corrCompare$teName <- gsub("[()]", "", corrCompare$teName)
-    
-    # for expression tab: avoid subscript out of bounds with empty expression
-    geneChoices <- intersect(corrRef$geneName, rownames(geneInput))
-    geneChoices <- intersect(corrCompare$geneName, geneChoices)
-    teChoices <- intersect(corrRef$teName, rownames(teInput))
-    teChoices <- intersect(corrCompare$teName, teChoices)
-    
-    runApp(
-        appDir=system.file("shinyGUI", package="TEKRABber"),
-        launch.browser=TRUE
-    )
-    
-    
+
+    source(system.file("app.R", package = "TEKRABber"))
+    launchGUI(DEresult, corrRef, corrCompare, metadata)
+
 }
--- inst/shinyGUI/global.R
+++
@@ -1,6 +0,0 @@
-library(TEKRABber)
-library(shiny)
-library(ggplot2)
-library(ggpubr)
-
-source("appTEKRABber.R")
